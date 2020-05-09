5e2f1cbd55bbe3560a565f2947a0886a0a833f55
H4RP3RK <kirsty.harper@zoho.com>
2020-05-09 11:33:01 +0000

    Moved JS map to Gallery, added embedded maps to about page, changed class names of headings

---
+++ assets/js/map.js
@@ -0,0 +1,9 @@
+            function initMap() {
+                var map = new google.maps.Map(document.getElementById("map"), {
+                    zoom: 10,
+                    center: {
+                        lat: 55.8617792,
+                        lng: -4.0054024
+                    }
+                });
+            }
