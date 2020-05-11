b3876fe3e70091d978a2af6f24519d36d04bc5fe
H4RP3RK <kirsty.harper@zoho.com>
2020-05-11 21:04:23 +0000

    wrote gallery js for next button on modal

--- assets/js/gallery.js
+++ assets/js/gallery.js
@@ -1,34 +1,29 @@
-function Gallery(gallery) {
-    if(!gallery) {
-        throw new Error("No Gallery Found");
-        return;
-    }
+function Modal(gallery) {
     const images = Array.from(gallery.querySelectorAll("img"));
     const modal = document.querySelector(".modal");
     const prevButton = modal.querySelector(".previous");
     const nextButton = modal.querySelector(".next");
     let currentImage;
-
-    function openModal() {
-        console.info("Opening Modal");
-    }
     
     function showImage(el) {
-        if(!el) {
-            console.info("no image");
-            return;
-        }
         console.log(el);
         modal.querySelector("img").src = el.src;
         modal.querySelector("h5").textContent = el.alt;
         modal.querySelector("figcaption p").textContent = el.dataset.caption;
         currentImage = el;
     }
+    
     function handleImageClick(event) {
         showImage(event.currentTarget);
     }
 
     images.forEach(image => image.addEventListener("click", handleImageClick))
+
+    function showNextImage() {
+        showImage(currentImage.parentElement.nextElementSibling.firstElementChild);
+    }
+
+    nextButton.addEventListener("click", showNextImage);
 }
 
-const gallery = Gallery(document.querySelector(".gallery"));
+const gallery = Modal(document.querySelector(".gallery"));
