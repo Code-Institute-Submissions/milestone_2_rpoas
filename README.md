bc6e2fd3f351c3eec87056659ad988f96a2adef8
H4RP3RK <kirsty.harper@zoho.com>
2020-05-14 21:06:07 +0000

    copied and amended form template from Signup page, started js to link contact form with Gmail account

--- README.md
+++ README.md
@@ -14,6 +14,7 @@
 * Bootstrap - grid layout, navbar, modal
 * JQuery - navbar
 * Google API - Gallery page
+* Gallery - Wesbos https://beginnerjavascript.com/
 
 --------------
 
---
+++ assets/js/contact.js
@@ -0,0 +1,13 @@
+function sendMail(contactForm) {
+    emailjs.send("gmail", "rpoas", {
+        "from_name": contactForm.fullName.value,
+        "from_email": contactForm.emailAddress.value,
+        "email_details": contactForm.query.value
+    })
+    .then(
+        function(response) {
+            console.log('SUCCESS!', response);
+        }, function(error) {
+       console.log('FAILED...', error);
+    });
+};
