d06832c9d38b4540588b3e88714562b758a55078
H4RP3RK <kirsty.harper@zoho.com>
2020-05-25 12:11:17 +0000

    events.html - added events list

--- assets/js/map.js
+++ assets/js/map.js
@@ -1,13 +1,3 @@
-function initMap() {
-    const mapProp = {
-        center: new google.maps.LatLng(55.8617792, -4.0054024),
-        zoom: 7,
-    }
-    const map = new google.maps.Map(document.querySelector("#photoMap"), mapProp);
-    
-    setMarkers(map);
-};
-
 const locations = [
     ["Hamilton", 55.7715968, -4.1186458],
     ["Motherwell", 55.7934441, -4.0486918],
@@ -15,13 +5,32 @@ const locations = [
     ["Airdrie", 55.8618126, -4.0402511]
 ];
 
-function setMarkers(map) {
+function initMap() {
+    const map = new google.maps.Map(document.querySelector("#photoMap"), {
+        zoom: 10,
+        center: {lat: 55.8617792, lng: -4.0054024}
+    });
+
+    function setMarkers(map) {
     for (let i=0; i < locations.length; i++) {
         let town = locations[i];
         let marker = new google.maps.Marker({
-            position: {lat: town[1], lng: town[2]},
             map: map,
-            title: town[0]
-        })
+            draggable: false,
+            animation: google.maps.Animation.DROP,
+            position: {lat: town[1], lng: town[2]},
+            title: town[0],
+            label: town[0]
+        });
+        marker.addListener("click", toggleBounce);    
     }
-}
+    function toggleBounce() {
+        if (marker.getAnimation() !== null) {
+            marker.setAnimation(null);
+        } else {
+            marker.setAnimation(google.maps.Animation.BOUNCE);
+        }
+    }
+    }
+    setMarkers(map);
+};
