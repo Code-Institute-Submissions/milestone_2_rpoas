7e7943c881513a175c6628d06c9b7f419353e2ab
H4RP3RK <kirsty.harper@zoho.com>
2020-05-26 19:44:11 +0000

    events - collapsable events list

--- assets/js/map.js
+++ assets/js/map.js
@@ -1,47 +1,60 @@
+/*------------------------------------------------------array of event objects--*/
+
 const events = [
     {
         title: "RPOAS Quiz Night",
         location: "Motherwell United Service Club, ML1 1TY",
         lat: 55.7841963,
         lng: -3.9906829,
-        datetime: "2020-06-29 20:00"
+        datetime: "2020-06-29 20:00",
+        description: `Get your thinking caps on for the quiz night. Bring family and friends, the more the merrier. The winners will be crowned Lanarkshire Branch Champions and win a top prize.`,
+        number: "One"
     },
     {
-        title: "Gin and Cultural Trip",
+        title: "Gin Trip in Auld Reekie",
         location: "National Museum of Scotland, Edinburgh, EH1 1JF",
         lat: 55.9469809,
         lng: -3.1927411,
-        datetime: "2020-07-13 19:30"
+        datetime: "2020-07-13 19:30",
+        description: `Join us for a gin tasting at the beautiful National Museum of Scotland followed by dinner.`,
+        number: "Two"
     },
     {
         title: "Drumpellier Police Dinner",
         location: "Drumpellier Golf Club, Coatbridge, ML5 1RX",
         lat: 55.8604836,
         lng: -4.0542339,
-        datetime: "2020-07-30 19:00"
+        datetime: "2020-07-30 19:00",
+        description: ``
     },
     {
         title: "RPOAS Lunch",
         location: "The Castle Rooms, Uddingston, G71 7PE",
         lat: 55.820283,
         lng: -4.0835657,
-        datetime: "2020-08-09 13:00"
+        datetime: "2020-08-09 13:00",
+        description: ``
     },
     {
         title: "Quiz Night",
         location: "Colville Park Club, Motherwell, ML1 4UG",
         lat: 55.8006031,
         lng: -3.9837373,
-        datetime: "2020-09-02 20:30"
+        datetime: "2020-09-02 20:30",
+        description: ``
     },
     {
         title: "Annual General Meeting",
         location: "Hamilton Caledonian Bowling Club, ML3 7BN",
         lat: 55.7753011,
         lng: -4.0311063,
-        datetime: "2020-10-01 14:00"
+        datetime: "2020-10-01 14:00",
+        description: ``
     },
 ];
+
+/*------------------------------------------------------Google map, markers and info windows--*/
+/*--Took inspiration from https://developers.google.com/maps/documentation/javascript/infowindows--*/
 
 function initMap() {
     const map = new google.maps.Map(document.querySelector("#photoMap"), {
@@ -64,48 +77,29 @@ function setMarkers(map) {
         <div class="form-section">
             <h3>${event.title}</h3>
             <h4>${event.datetime}</h4>
-            <h5>${event.location}</h5>
+            <h4>${event.location}</h4>
         </div>`;
         let infoWindow = new google.maps.InfoWindow({
             content: windowContent
         });
         eventsList.insertAdjacentHTML("beforeend", `
-        <li class="form-section">
-            <h3>${event.title}</h3>
-            <h4>${event.datetime}</h4>
-            <h5>${event.location}</h5>
-        </li>
+        <div class="form-section main-text">
+            <div id="heading${event.number}">
+                <div data-toggle="collapse" data-target="#collapse${event.number}" aria-expanded="false" aria-controls="collapse${event.number}">
+                    <h3>${event.title}</h3>
+                    <h4>${event.datetime}</h4>
+                    <h5>${event.location}</h5>
+                </div>
+            </div>
+            <div id="collapse${event.number}" class="collapse" aria-labelledby="heading${event.number}" data-parent="#accordian">
+                <div class="main-text">
+                    ${event.description}
+                </div>
+            </div>
+        </div>
         `);
         marker.addListener("click", function() {
             infoWindow.open(map, marker);
         });
     });
 };
-
-
-
-/*
-    function setMarkers(map) {
-    for (let i=0; i < locations.length; i++) {
-        let town = locations[i];
-        let marker = new google.maps.Marker({
-            map: map,
-            draggable: false,
-            animation: google.maps.Animation.DROP,
-            position: {lat: town[1], lng: town[2]},
-            title: town[0],
-            label: town[0]
-        });
-        marker.addListener("click", toggleBounce);    
-    }
-    function toggleBounce() {
-        if (marker.getAnimation() !== null) {
-            marker.setAnimation(null);
-        } else {
-            marker.setAnimation(google.maps.Animation.BOUNCE);
-        }
-    }
-    }
-    setMarkers(map);
-};
-*/
